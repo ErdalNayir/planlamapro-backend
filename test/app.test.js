0bb3b7bb74ce8bc496aa727fd396d757bd3d20e3
Erdal Nayir <erdal.nayir2001@gmail.com>
2023-08-01 00:06:52 +0300

    Update app.test.js

--- test/app.test.js
+++ test/app.test.js
@@ -5,6 +5,5 @@ describe("Test Express.js API", () => {
   test("POST /user/login", async () => {
     const response = await request(app).post("/user/login");
     expect(response.status).toBe(500);
-    expect(response.body).toHaveProperty(undefined);
   });
 });
